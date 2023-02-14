36a2d48e2d5cc3af6cbd8712e9ba7177d90cb9a6
gaston <gastonmonasterio1998@gmail.com>
2023-02-14 16:45:12 -0300

    preparando el despliegue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('./config/conexion');
 const express = require('express')
-const port = (process.env.port || 3000)
+const port = (process.env.PORT || 3000)
 
 // express
 const app = express()
